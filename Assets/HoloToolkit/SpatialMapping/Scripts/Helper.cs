be560145014f20337ffca6baabfbc97dc46ca952
Seyoung <seyoung.arts.park@protonmail.com>
2017-05-17 15:04:25 +0300

    Add custom debug

---
+++ Assets/HoloToolkit/SpatialMapping/Scripts/Helper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.HoloToolkit.SpatialMapping.Scripts
+{
+    public class Helper
+    {
+        public enum DebugType{ ValueCheck, Weird ,Error}
+
+        public void debug(String line, DebugType type)
+        {
+            string msgPrefix = null;
+            if (type == DebugType.ValueCheck)
+            {
+                msgPrefix = "<color=green>ValueCheck:</color>";
+            }
+            else if (type == DebugType.Weird)
+            {
+                msgPrefix = "<color=orange>Weird:</color>";
+            }
+            else if (type == DebugType.Error)
+            {
+                msgPrefix = "<color=red>Error:</color>";
+            }
+
+            if (msgPrefix != null)
+            {
+                Debug.Log(msgPrefix);
+                Debug.Log(line)
+            }
+           
+        }
+    }
+}
